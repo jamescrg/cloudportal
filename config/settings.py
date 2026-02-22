d0391e37ae9d853a59fd95eb43a5a041cfdb0a62
James Craig <jamespcraig@gmail.com>
2026-02-22 17:01:28 +0000

    Rebrand from CloudPortal to MinHome

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,6 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 # trusted origins for CSRF
 CSRF_TRUSTED_ORIGINS = [
     "https://dev.minhome.app",
-    "https://dev.cloudportal.link",
     "https://server-dev.site:8000",
 ]
 
